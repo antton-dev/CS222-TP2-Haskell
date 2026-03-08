9644c750ac170d36c9163e9a7ca4ebd3e5bb49ab
antton-dev <antton.chevrier@etik.com>
2026-03-08 19:25:45 +0100

    fix #2 and fix non-exhaustive error on meme_couleur

--- Tarot.hs
+++ Tarot.hs
@@ -18,7 +18,7 @@ comparer (Standard v1 c1) (Standard v2 c2) | c1==c2    =  v1 > v2
 meme_couleur :: Couleur -> Carte -> Bool
 meme_couleur c1 ((Standard _ c2 )) | c1==c2    =  True               
                                    | otherwise =  False
-
+meme_couleur _ _ = False
 
 atout_plus_grand :: Int -> Carte -> Bool
 atout_plus_grand v1 (Atout v2) | v2 > v1   = True
@@ -33,4 +33,21 @@ atout _ = False
             | otherwise  = l2
 
 cartes_possibles_hors_excuse :: Carte -> [Carte] -> [Carte]
-cartes_possibles_hors_excuse (Standard c v) main = filter (meme_couleur c) main
+cartes_possibles_hors_excuse (Standard _ c) deck = filter (meme_couleur c)  deck ||| filter atout deck ||| deck 
+cartes_possibles_hors_excuse (Atout v ) deck = filter (atout_plus_grand v) deck ||| filter atout deck ||| deck
+
+
+cartes_possibles :: Carte -> [Carte] -> [Carte]
+cartes_possibles played deck = sans_excuse ++ excuses 
+    where 
+        excuses = filter (est_excuse) deck 
+        deck_restant = filter (not_excuse) deck
+
+        sans_excuse = cartes_possibles_hors_excuse played deck_restant 
+
+
+        est_excuse Excuse = True
+        est_excuse _      = False
+
+        not_excuse Excuse = False
+        not_excuse _      = True
